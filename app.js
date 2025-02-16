41706a707bf5beff338c6acd99719135857d3e3e
victorlistapad <victorlistapad@gmail.com>
2025-02-15 21:59:16 -0500

    New: - Created HTML layout - Created styles - Created logic to fetch new deck of cards from deckofcardsapi - Created logic to draw two random cards from fetched deck - Created logic to display chosen cards and compare their values - Created logic to display current scores for computer and player - Created logic to prevent user from clicking the draw button before deck was fetched or if the are no remaining cards

--- app.js
+++ app.js
@@ -1,24 +1,75 @@
 let deckId;
+let playerScore = 0;
+let computerScore = 0;
+const remainingCards = document.querySelector(".remaining-cards");
 
-function handleClick() {
-  fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
+document.querySelector("#new-deck").addEventListener("click", () => {
+  fetch(`https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`)
     .then((res) => res.json())
     .then((data) => {
-      console.log(data);
       deckId = data.deck_id;
+      remainingCards.textContent = `Cards Remaining: ${data.remaining}`;
+      document.querySelector(".draw-btn").disabled = false;
     });
-}
+});
 
-document.getElementById("new-deck").addEventListener("click", handleClick);
+document.querySelector(".draw-btn").addEventListener("click", () => {
+  drawCards(deckId);
+});
 
-document.getElementById("draw-cards").addEventListener("click", () => {
-  fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
+function drawCards(deckId) {
+  fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`)
     .then((res) => res.json())
     .then((data) => {
       console.log(data.cards);
-      document.getElementById("cards").innerHTML = `
-                <img src=${data.cards[0].image} class="card" />
-                <img src=${data.cards[1].image} class="card" />
-            `;
+      remainingCards.textContent = `Cards Remaining: ${data.remaining}`;
+      renderCards(data.cards[0], data.cards[1]);
+      compareCards(data.cards[0].value, data.cards[1].value);
+      if (data.remaining === 0) {
+        document.querySelector(".draw-btn").disabled = true;
+      }
     });
-});
+}
+
+function compareCards(compCard, playerCard) {
+  const playerScoreText = document.querySelector("#player-score");
+  const computerScoreText = document.querySelector("#computer-score");
+  const cardValues = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "JACK",
+    "QUEEN",
+    "KING",
+    "ACE",
+  ];
+  const playerScoreIndex = cardValues.indexOf(playerCard);
+  const compScoreIndex = cardValues.indexOf(compCard);
+  const currentResults = document.querySelector(".current-results");
+  if (playerScoreIndex > compScoreIndex) {
+    playerScore++;
+    currentResults.textContent = `Player Scores 1 point`;
+  } else if (playerScoreIndex < compScoreIndex) {
+    computerScore++;
+    currentResults.textContent = `Computer Scores 1 point`;
+  } else {
+    currentResults.textContent = `Draw`;
+  }
+  playerScoreText.textContent = `Player score: ${playerScore}`;
+  computerScoreText.textContent = `Computer score: ${computerScore}`;
+}
+
+function renderCards(compCard, playerCard) {
+  document.querySelector("#player-card").innerHTML = `
+    <img src="${playerCard.images.png}" alt="${playerCard.value} of ${playerCard.suit}" />
+  `;
+  document.querySelector("#computer-card").innerHTML = `
+    <img src="${compCard.images.png}" alt="${compCard.value} of ${compCard.suit}" />
+  `;
+}
