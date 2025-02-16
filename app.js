ceecf67ee34c79a09fadc91d49de27cdc2f8871b
victorlistapad <victorlistapad@gmail.com>
2025-02-15 23:13:48 -0500

    Updated: - Refactored compareCards function by adding game scores inside function closure. - Removed player and computer score DOM element declarations - Removed player and computer score variables from global scope New: - Created logic to display player or computer score advantage

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
+// GLOBAL VARIABLES
+const draw = compareCards();
 let deckId;
-let playerScore = 0;
-let computerScore = 0;
 const remainingCards = document.querySelector(".remaining-cards");
 
+// GET A NEW DECK
 document.querySelector("#new-deck").addEventListener("click", () => {
   fetch(`https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`)
     .then((res) => res.json())
@@ -13,10 +14,12 @@ document.querySelector("#new-deck").addEventListener("click", () => {
     });
 });
 
+// ADD EVENT LISTENER TO DRAW CARDS
 document.querySelector(".draw-btn").addEventListener("click", () => {
   drawCards(deckId);
 });
 
+// DRAW CARDS LOGIC
 function drawCards(deckId) {
   fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`)
     .then((res) => res.json())
@@ -24,16 +27,16 @@ function drawCards(deckId) {
       console.log(data.cards);
       remainingCards.textContent = `Cards Remaining: ${data.remaining}`;
       renderCards(data.cards[0], data.cards[1]);
-      compareCards(data.cards[0].value, data.cards[1].value);
+      draw(data.cards[0].value, data.cards[1].value);
       if (data.remaining === 0) {
         document.querySelector(".draw-btn").disabled = true;
       }
     });
 }
 
-function compareCards(compCard, playerCard) {
-  const playerScoreText = document.querySelector("#player-score");
-  const computerScoreText = document.querySelector("#computer-score");
+// COMPARE TWO CARDS
+function compareCards() {
+  const scores = [0, 0];
   const cardValues = [
     "2",
     "3",
@@ -49,22 +52,39 @@ function compareCards(compCard, playerCard) {
     "KING",
     "ACE",
   ];
-  const playerScoreIndex = cardValues.indexOf(playerCard);
-  const compScoreIndex = cardValues.indexOf(compCard);
-  const currentResults = document.querySelector(".current-results");
-  if (playerScoreIndex > compScoreIndex) {
-    playerScore++;
-    currentResults.textContent = `Player Scores 1 point`;
-  } else if (playerScoreIndex < compScoreIndex) {
-    computerScore++;
-    currentResults.textContent = `Computer Scores 1 point`;
-  } else {
-    currentResults.textContent = `Draw`;
-  }
-  playerScoreText.textContent = `Player score: ${playerScore}`;
-  computerScoreText.textContent = `Computer score: ${computerScore}`;
+  return function (compCard, playerCard) {
+    const playerScoreIndex = cardValues.indexOf(playerCard);
+    const compScoreIndex = cardValues.indexOf(compCard);
+    const currentResults = document.querySelector(".current-results");
+    if (playerScoreIndex > compScoreIndex) {
+      scores[1]++;
+      currentResults.textContent = `You score 1 point`;
+    } else if (playerScoreIndex < compScoreIndex) {
+      scores[0]++;
+      currentResults.textContent = `Computer scores 1 point`;
+    } else {
+      currentResults.textContent = `Tie`;
+    }
+    currentResults.textContent += compareTotalScores(scores[0], scores[1]);
+    document.querySelector(
+      "#player-score"
+    ).textContent = `My score: ${scores[1]}`;
+    document.querySelector(
+      "#computer-score"
+    ).textContent = `Computer score: ${scores[0]}`;
+  };
 }
 
+// CHECK POINT ADVANTAGE
+function compareTotalScores(compScore, playerScore) {
+  return compScore > playerScore
+    ? `\r\nComputer is ${compScore - playerScore} point(s) ahead`
+    : compScore < playerScore
+    ? `\r\nYau are ${playerScore - compScore} point(s) ahead`
+    : `\r\n It is a tie`;
+}
+
+// RENDER CARDS
 function renderCards(compCard, playerCard) {
   document.querySelector("#player-card").innerHTML = `
     <img src="${playerCard.images.png}" alt="${playerCard.value} of ${playerCard.suit}" />
